8cbc5c1c51d70991536d5f67cb7cb90adda35552
Brandon Chang <brandonchang@Brandons-MacBook-Pro-2.local>
2017-12-10 23:21:12 -0800

    added in code for search test and finding the lowest priced item

--- features/practice/step_definitions/grailed.rb
+++ features/practice/step_definitions/grailed.rb
@@ -15,25 +15,81 @@ And(/^I log into my grailed account$/) do
   step "I log into my grailed account from the cell"
 end
 
-And(/^I click on the footwear header$/) do
-  puts "checking grailed website..." if @verbose
-  find("div.sub-header-links").all("a")[3].click
+And(/^I click on the "([^"]*)" category header$/) do |header|
+  puts "checking all top category headers..." if @verbose
+
+  case header
+
+    when "outerwear" then find("div.sub-header-links").all("a")[0].click
+
+    when "tops" then find("div.sub-header-links").all("a")[1].click
+
+    when "bottoms" then find("div.sub-header-links").all("a")[2].click
+
+    when "footwear" then find("div.sub-header-links").all("a")[3].click
+
+    when "sneakers" then find("div.sub-header-links").all("a")[4].click
+
+    when "tailoring" then find("div.sub-header-links").all("a")[5].click
+
+    when "accessories" then find("div.sub-header-links").all("a")[6].click
+
+  else
+    fail "You selected a category header that does not exist!"
+  end
+  
+end
+
+Then(/^I verify the "([^"]*)" category is clicked under the category filter$/) do |category|
+  begin
+    side_filters = find("h3.filter-toggle", :text=>"SHOW FILTERS")
+    assert side_filters
+    page.first("div.filter-toggle-wrapper").click 
+  rescue
+    puts "Side filters are visible. Continuing..." if @verbose
+  ensure 
+    category_choice = find("div.categories-wrapper").find("div.filter-bottom")
+    assert category_choice.find("div.#{category}-wrapper").find("p.toggle-all.active-indicator.active")
+  end 
 end
 
 And(/^I search for "([^"]*)" and I put sizes filter into view$/) do |search|
-  puts assert find("input.search", :wait=>0)
-
-  find("input.search", :wait=>0).set "#{search}"
+  find("div.feed-settings-bar.container").find("input.search", :wait=>0).set "#{search}"
   sleep 0.5
 
   scroll_to(page.find("div.sizes-wrapper").find("h3", :text=>"SIZE"))
 end
 
-And(/^I select size 11$/) do
-  find("span[data-reactid='.4.2.3.0.0.0.4.1.$Footwear.0.0.1']").click
-  sleep 0.5
-  find("p[data-reactid='.4.2.3.0.0.0.4.1.$Footwear.1.$11']").click
-  sleep 0.5
+And(/^I select size "([^"]*)" within the "([^"]*)" filter$/) do |size, filter|
+  size_filter = find("div.sizes-wrapper")
+
+  size_filter.first("span", :text=>filter).find("span.triangle").click
+
+  case filter 
+
+    when "Tops & Outerwear"
+      find("p[data-reactid='.4.2.3.0.0.0.4.1.$Tops & Outerwear.1.$#{size}']").click
+      sleep 0.5
+
+    when "Bottoms & Pants"
+      find("p[data-reactid='.4.2.3.0.0.0.4.1.$Bottoms & Pants.1.$#{size}']").click
+      sleep 0.5
+
+    when "Footwear"
+      find("p[data-reactid='.4.2.3.0.0.0.4.1.$Footwear.1.$#{size}']").click
+      sleep 0.5
+
+    when "Tailoring"
+      find("p[data-reactid='.4.2.3.0.0.0.4.1.$Tailoring.1.$#{size}']").click
+      sleep 0.5
+
+    when "Accessories"
+      find("p[data-reactid='.4.2.3.0.0.0.4.1.$Accessories.1.$#{size}']").click
+      sleep 0.5
+
+  else
+    fail "You selected a size filter that does not exist!"
+  end
 end
 
 And(/^I click on ask the seller a question$/) do
@@ -66,7 +122,6 @@ end
 
 
 And(/^I test the category headers and verify the href$/) do
-  # assert find("div.sub-header-links.display-flex")
 
   puts "starting to check the category links..." if @verbose  
 
@@ -81,7 +136,6 @@ And(/^I test the category headers and verify the href$/) do
 
     category_links = category_links[i]
  
-    # sleep 1
     @href = category_links[:href]  
     puts "found the link href: #{@href}" if @verbose 
     
@@ -101,54 +155,9 @@ And(/^I test the category headers and verify the href$/) do
   end 
 end
 
-
-
-# When(/^I randomly test the category headers$/) do
-#   i = 0
-#   puts "Starting to check related links..."
-  
-#   loop do 
-
-#     links_array = all("div.header-link a", :wait=>0)
-#     len = links_array.size
-#     all_links = links_array[i]
- 
-#     # sleep 1
-#     @related_href = all_links[:href] 
-     
-#     puts "found the link href: #{@related_href}" if @verbose 
-
-
-#     # all_links.click 
-#     # sleep 1
-#     # wait_for_page
-
-#     # assert_current_path @related_href, url: true #check the new page that loads matches the url from the href
-    
-
-#     i += 1
-#     puts "check len: #{len}" if @verbose
-#     puts "check i: #{i}" if @verbose
-
-#     if (i >= len)
-#       break
-#     end
-    
-#     # sleep 1
-#     # step "I go back to the previous page"
-#     # sleep 1
-
-#   end
-# end
-
 When(/^I go back to the previous page$/) do
   page.evaluate_script('window.history.back()')
   # sleep 0.5
-end
-
-And(/^I look for 770 in product cell$/) do
-  # assert find("div.feed-item", :text=>"770")
-  assert all("div.feed-item")[3].find("span", :text=>"770")
 end
 
 And(/^I check for the cheapest listing price$/) do
@@ -157,19 +166,16 @@ And(/^I check for the cheapest listing price$/) do
 
   i = 0
   feed_cell = all("div.feed-item")
-  # feed_cell = all("div.feed-item")[i]
   all_prices = all("div.listing-price")
   @price_array = []
  
   all_prices.each do |price|
 
-    @cell_price=price.text.gsub("$","") 
+    @cell_price = price.text.gsub("$","") 
     @price_array = @price_array.push(@cell_price)
     @price_array = @price_array.map(&:to_i)
 
-    # i+=1
-
-    puts "Found a listing price of #{price.text}" if @verbose # in listing #{i}
+    # puts "Found a listing price of #{price.text}" if @verbose # in listing #{i}
   end
 
   puts @price_array if @verbose
@@ -177,41 +183,40 @@ And(/^I check for the cheapest listing price$/) do
   puts @price_array.min if @verbose
   puts "Found the lowest price of #{@price_array.min}" if @verbose
 
-  puts "=========================================================="
+  puts "================================================================================================="
   
   loop do 
     
-    cell_link = all("div.feed-item a", :wait=>0)
-    len = cell_link.size
-    browse_links = cell_link[i]
+    listing_link = all("div.feed-item a", :wait=>0)
+    len = listing_link.size
+    browse_links = listing_link[i]
 
-    break_cell = all("div.feed-item", :wait=>0)
-    break_link = break_cell[i]
+    step "I scroll down to see more products"
 
-    puts i
-    @a_href = browse_links[:href]  
-    puts "found the link href: #{@a_href}" if @verbose 
+    # puts i if @verbose
+    # @a_href = browse_links[:href]  
+    # puts "found the link href: #{@a_href}" if @verbose 
 
     i+=1
 
     begin
-      if assert break_link.find("span", :text=>"#{@price_array.min}", :wait=>0)
+      if assert browse_links.find("span", :text=>"#{@price_array.min}", :wait=>0)
         browse_links.click
         break
       end
     rescue
+      puts "Searching for listing with the lowest price..." if @verbose
     ensure
       loop
     end
 
-    if (i >= len)
+    if (i >= 50)
       break
     end
 
   end
-  # sleep 10
   url = URI.parse(current_url)
-  puts url
+  puts "We found our cheapest listing: #{url}"
 end
 
 
@@ -243,31 +248,6 @@ end
 
 Then(/^I scroll down to see more products$/) do
   page.execute_script "window.scrollBy(0,3000)"
-  wait_for_ng2
-end
-
-
-
-
-
-
-
-###############################################################################################
-
-Given(/^I go to the Shopstyle homepage$/) do
- if(@browser!=true)
-    selenium_init
-    Capybara.app_host = @shopstyle_host
-  end
-
-  visit('')
-
-  #dismiss the currency modal on middle-east locale
-  # if(@shopstyle_host.include?("me"))
-  #   dismiss_currency_modal
-  # end
-
-  # step "I am on the homepage"
 end
 
 ############helper############
